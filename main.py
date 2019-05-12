fb3ec535cb25a402ca3a3092dc9a41e1428f340e
Stadzior <Stadzior@gmail.com>
2019-05-12 16:08:24 +0200

    successfull prediction, yay!

--- main.py
+++ main.py
@@ -11,15 +11,15 @@ data_gen_args = dict(rotation_range=0.2,
                     zoom_range=0.05,
                     horizontal_flip=True,
                     fill_mode='nearest')
-trainGene = trainGenerator(2,'data/train','image','label',data_gen_args,save_to_dir = None)
+trainGene = trainGenerator(1,'data/train','image','label',data_gen_args,save_to_dir = None)
 
 model = unet()
 model_checkpoint = ModelCheckpoint('unet.hdf5', monitor='loss',verbose=1, save_best_only=True)
 print("before fit generator")
-model.fit_generator(trainGene,steps_per_epoch=50,epochs=3,callbacks=[model_checkpoint])
+model.fit_generator(trainGene,steps_per_epoch=500,epochs=2,callbacks=[model_checkpoint])
 print("after fit generator")
 
-testGene = testGenerator("data/test", 1)
-results = model.predict_generator(testGene, 1,verbose=1)
+testGene = testGenerator("data/test", 3)
+results = model.predict_generator(testGene, 3,verbose=1)
 print("after predict generator")
 saveResult("data/test",results)
